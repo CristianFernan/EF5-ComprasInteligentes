32c889bbe38a5d779d5e794d11ed68ad9c798915
Luismerino123 <00083723@uca.edu.sv>
2024-07-08 22:05:36 -0600

    Agregando interfaz grafica de reportes

---
+++ ComprasInteligentes/src/main/java/org/example/comprasinteligentes/controllers/ReportesController.java
@@ -0,0 +1,27 @@
+package org.example.comprasinteligentes.controllers;
+
+import javafx.fxml.FXML;
+import org.example.comprasinteligentes.Conexion;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import org.example.comprasinteligentes.clases.CompraCustom;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.sql.Date;
+
+
+
+public class ReportesController {
+    @FXML
+    private void onBtnClientesClick(){ return;}
+    @FXML
+    private void onBtnComprasClick(){return;}
+
+}
